e7d3a3ad6f2805325c6a0fb1b64624b1b60f8e9a
Toby Vincent John <tobyvincentjohn@gmail.com>
2025-02-01 22:01:34 +0530

    added the first script and the secrets

---
+++ trial.py
@@ -0,0 +1,8 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Explain how AI works")
+print(response.text)
